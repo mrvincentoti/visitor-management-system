55ae02419688174ba694751849b0e184934784fc
codewith-zach <92583144+codewith-zach@users.noreply.github.com>
2022-08-25 10:54:33 +0100

    changed routers

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -17,14 +17,14 @@ const { checkToken } = require('../../auth/token_validation');
 
 
 //Now we have to define the route for all the controllers
-router.post('/createVisitors', checkToken, createVisitors);    //we are passing the request to 'creatUser' in the user.controller
-router.get('/getVisitors', checkToken, getVisitors);
-router.get('/getVisitorsByVisitorId:id', checkToken, getVisitorsByVisitorId);
-router.patch('/updateVisitors', checkToken, updateVisitors);
-router.delete('/deleteVisitors', checkToken, deleteVisitors);
-router.get('/getUserByUserId:id', checkToken, getUserByUserId);
-router.patch('/updateUsers', checkToken, updateUser);
-router.delete('/deleteUser', checkToken, deleteUser);
+router.post('/visitor/createVisitors', checkToken, createVisitors);    //we are passing the request to 'creatUser' in the user.controller
+router.get('/visitor/getVisitors', checkToken, getVisitors);
+router.get('/visitor/getVisitorsByVisitorId:id', checkToken, getVisitorsByVisitorId);
+router.patch('/visitor/updateVisitors', checkToken, updateVisitors);
+router.delete('/visitor/deleteVisitors', checkToken, deleteVisitors);
+router.get('/user/getUserByUserId:id', checkToken, getUserByUserId);
+router.patch('/user/updateUsers', checkToken, updateUser);
+router.delete('/user/deleteUser', checkToken, deleteUser);
 router.post('/user/login', login);   //used for login
 router.get('/user/getUsers', getUsers);
 router.post('/user/createUsers', createUsers);
